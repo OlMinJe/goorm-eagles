bb724e460da23b5e6bf00031539b9e3472c68eb5
2SIONN <leesion2024@gmail.com>
2025-08-21 12:02:51 +0900

    fix: Set default Content-Type header to application/json

--- goorm-project/src/api/http.js
+++ goorm-project/src/api/http.js
@@ -5,5 +5,5 @@ const BASE_API_URL = import.meta.env.DEV ? '/api' : import.meta.env.VITE_API_URL
 export const instance = axios.create({
   baseURL: BASE_API_URL,
   timeout: 1000,
-  headers: { 'X-Custom-Header': 'foobar' },
+  headers: { 'Content-Type': 'application/json' },
 })
