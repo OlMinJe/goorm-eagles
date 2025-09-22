74d83cd1d98420cde37208de55648c411d887560
OlMinJe <Ol.MinJe@gmail.com>
2025-09-22 09:44:16 +0900

    [web] 실습 전 세팅

--- goorm-next-app/src/app/bad/page.tsx
+++ goorm-next-app/src/app/bad/page.tsx
@@ -1,28 +1,20 @@
 import ContextAbuse from '@/components/bad/ContextAbuse'
-import CTA from '@/components/bad/CTA'
-import EffectAbuse from '@/components/bad/EffectAbuse'
-import Features from '@/components/bad/Features'
-import Gallery from '@/components/bad/Gallery'
-import Hero from '@/components/bad/Hero'
-import KeyedList from '@/components/bad/KeyedList'
-import MemoAbuse from '@/components/bad/MemoAbuse'
-import UnstableProps from '@/components/bad/UnstableProps'
 
 export const dynamic = 'force-dynamic'
 
 export default function BadPage() {
   return (
     <>
-      <Hero />
-      <Features />
+      {/* <Hero /> */}
+      <ContextAbuse />
+
+      {/* <Features />
       <Gallery />
       <CTA />
-
-      <ContextAbuse />
       <UnstableProps />
       <KeyedList />
       <EffectAbuse />
-      <MemoAbuse />
+      <MemoAbuse /> */}
     </>
   )
 }
